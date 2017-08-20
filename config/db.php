1741f5983e11675e839b0e26b336fc5b3f92eb9c
Yehor Smoliakov <egorsmkv@gmail.com>
2017-08-20 17:58:03 +0300

    Added model caching

--- config/db.php
+++ config/db.php
@@ -2,15 +2,8 @@
 
 // Connection settings to database
 
-if (false === YII_DEBUG) { // PRODUCTION
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=',
-        'username' => '',
-        'password' => '',
-        'charset' => 'utf8',
-    ];
-} else { // DEBUG
+// DEBUG
+if (YII_DEBUG) {
     return [
         'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=localhost;dbname=placehold',
@@ -19,3 +12,17 @@ if (false === YII_DEBUG) { // PRODUCTION
         'charset' => 'utf8',
     ];
 }
+
+// PRODUCTION
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Caching
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
+];
